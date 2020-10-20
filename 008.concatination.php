bd30f1fedaaeaee5e4ddfef6736a132ca3280f42
Abdullah Sajjad <73114676+abdullahsajjadpro@users.noreply.github.com>
2020-10-20 09:15:35 +0500

    Add files via upload

--- 008.concatination.php
+++ 008.concatination.php
@@ -13,4 +13,4 @@ $sumOfAges = $ageTwo + $ageTwo;
 
 // Concatinating by using (.) Operator.
 $result = "Names Are {$nameOne} and {$nameTwo} with Ages " . $ageOne . " and " . $ageTwo . " and the Sum of Both Ages is " . $sumOfAges;
-echo $result;
+echo $result; // Output Is:- Names Are Abdullah Sajjad and Abdu Bakar with Ages 15 and 14 and the Sum of Both Ages is 28
