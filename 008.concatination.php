5f9d2e43753f1ed931eda161249a29ee74e88f6f
Abdullah Sajjad <73114676+abdullahsajjadpro@users.noreply.github.com>
2020-10-20 09:13:59 +0500

    Add files via upload

---
+++ 008.concatination.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Concatination In PHP:-
+ *  Concatinating means we are joining Strings with Anything or Numbers with Anything.
+ */
+
+$nameOne = "Abdullah Sajjad";
+$ageOne = 15;
+$nameTwo = "Abdu Bakar";
+$ageTwo = 14;
+$sumOfAges = $ageTwo + $ageTwo;
+
+// Concatinating by using (.) Operator.
+$result = "Names Are {$nameOne} and {$nameTwo} with Ages " . $ageOne . " and " . $ageTwo . " and the Sum of Both Ages is " . $sumOfAges;
+echo $result;
